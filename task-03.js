a60d0c963aee919e2e23904a873acb58fbc896af
Ali Burzhi <aliburzhi033@gmail.com>
2021-08-02 13:38:19 +0300

    task 5 done

--- task-03.js
+++ task-03.js
@@ -1,35 +1,56 @@
-const images = [
-  {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
-  },
-  {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
-  },
-  {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
-  },
-];
+// const images = [
+//   {
+//     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+//     alt: 'White and Black Long Fur Cat',
+//   },
+//   {
+//     url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+//     alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+//   },
+//   {
+//     url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+//     alt: 'Group of Horses Running',
+//   },
+// ];
 
-const galleryListEl = document.querySelector('#gallery');
+// const galleryListEl = document.querySelector('#gallery');
 
-galleryListEl.style.display = 'flex';
-galleryListEl.style.alignItems = 'center';
-galleryListEl.style.justifyContent = 'space-between';
+// galleryListEl.style.display = 'flex';
+// galleryListEl.style.alignItems = 'center';
+// galleryListEl.style.justifyContent = 'space-between';
 
-const createListItem  = images.map(image => {
+// const createListItem  = images.map(image => {
     
-    const itemEl = document.createElement('li');
-    itemEl.style.listStyle = 'none';
-    itemEl.insertAdjacentHTML(
-        'afterbegin',
-        `<img src='${image.url}' alt='${image.alt}' width='400px'>`
-    );
-    return itemEl;
+//     const itemEl = document.createElement('li');
+//     itemEl.style.listStyle = 'none';
+//     itemEl.insertAdjacentHTML(
+//         'afterbegin',
+//         `<img src='${image.url}' alt='${image.alt}' width='400px'>`
+//     );
+//     return itemEl;
     
 
-});
+// });
 
-galleryListEl.append(...createListItem);
+// galleryListEl.append(...createListItem);
+
+
+
+// TASK 05 ================================================================================
+// const refs = {
+//   input: document.querySelector('#name-input'),
+//   title: document.querySelector('h1 > span'),
+// };
+
+// refs.input.addEventListener('input', makeSametext);
+
+
+// function makeSametext(event) {
+//   if (event.currentTarget.value === '') {
+//     refs.title.textContent = 'незнакомец';
+//   } else {
+//       refs.title.textContent = event.currentTarget.value;
+
+//  }
+// }
+// ====================================================================================================
